0e73931a0acefc41e001387cc9252f397dec0e11
ithamarSpitz <itas12@gmail.com>
2021-11-22 21:33:25 +0200

    replace 0

--- my_mat.c
+++ my_mat.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+void replace_zeros(int mat[10][10]){
+  for (int i = 0; i < 10; i++){
+    for(int j = 0; j < 10 ; j++){
+      if(i != j && mat[i][j] == 0) 
+        mat[i][j] = 10000000;
+    }
+  }
+}
+
+
 void floydWarshall(int mat[10][10]) {
   int matrix[10][10], i, j, k;
     for (k = 0; k < 10; k++) {
@@ -7,7 +17,6 @@ void floydWarshall(int mat[10][10]) {
             for (j = 0; j < 10; j++) {
                 if (mat[i][k] + mat[k][j] <= mat[i][j])
                 matrix[i][j] = mat[i][k] + mat[k][j];
-                
       }
     }
   }
