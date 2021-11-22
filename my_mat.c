8d8b32067cd4f9111bc342012ccd54b094d41be9
edenmer <edenmer5@gmail.com>
2021-11-22 22:12:10 +0200

    fix

--- my_mat.c
+++ my_mat.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 void replace_zeros(int mat[10][10]){
   for (int i = 0; i < 10; i++){
@@ -9,15 +10,31 @@ void replace_zeros(int mat[10][10]){
   }
 }
 
-
-void floydWarshall(int mat[10][10]) {
+void floydWarshall(int mat[10][10]){
   int matrix[10][10], i, j, k;
-    for (k = 0; k < 10; k++) {
-        for (i = 0; i < 10; i++) {
-            for (j = 0; j < 10; j++) {
-                if (mat[i][k] + mat[k][j] <= mat[i][j])
-                matrix[i][j] = mat[i][k] + mat[k][j];
+  for (int i = 0; i < 10; i++){
+      for (int j = 0; j < 10; j++){
+          matrix[i][j] = mat[i][j];
       }
-    }
   }
+  replace_zeros(*matrix);
+
+  for (k = 0; k < 10; k++) {
+      for (i = 0; i < 10; i++) {
+          for (j = 0; j < 10; j++) {
+              if (mat[i][k] + mat[k][j] <= mat[i][j])
+                matrix[i][j] = mat[i][k] + mat[k][j];
+              }
+          }
+      }
+}
+
+bool ifRoute(int i, int j, int mat[10][10]){
+  if(mat[i][j]!=0 && mat[i][j]!=10000000) return true;
+  else return  false;
+}
+
+void printRoute(int i, int j, int mat[10][10]){
+  if(mat[i][j]!=0 && mat[i][j]!=10000000) printf("%d", mat[i][j]);
+  else printf("%d", -1);
 }
