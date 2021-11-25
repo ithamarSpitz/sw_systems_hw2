a91f98d8415db71f37d820162d588538c081ab28
ithamarSpitz <itas12@gmail.com>
2021-11-25 21:59:04 +0200

    fix main

--- main.c
+++ main.c
@@ -1,87 +1,35 @@
 #include <stdio.h>
 #include "my_mat.h"
 
-void replace2zeros(int mat[10][10]){
-  for (int i = 0; i < 10; i++){
-    for(int j = 0; j < 10 ; j++){
-        mat[i][j] = 0;
-    }
-  }
+void fill_mat(int mat[10][10]){
+    for(int i = 0; i < 100; i++)
+        scanf("%d", &mat[i/10][i%10]);
+}
+
+void route(int to_print, int mat[10][10]){
+    int source;
+    int dest;
+    scanf("%d", &source);
+    scanf("%d", &dest);
+    if(to_print)
+        printRoute(source, dest, mat);
+    else
+        ifRoute(source, dest, mat);
 }
 
 int main(){
-    int a = 0;
-    int b = 0;
-    int c = 0;
     int mat[10][10];
-    int k = 0;
-    int j = 0;
-    char str[10000];
-    scanf("%s", str);
-    int index = 0;
-    int source = -1;
-for (int i = 0; i < 1000; i++)
-{
-    char temp = str[i];
-    if(temp == 'D')
-        break;
-    if(temp == 'A'){
-        a = 1;
-        b = 0;
-        c = 0;
-        replace2zeros(mat);
-        continue;
-    }
-    if(temp == 'B'){
-        a = 1;
-        b = 0;
-        c = 0;
-        continue;
-    }
-    if(temp == 'C'){
-        a = 1;
-        b = 0;
-        c = 0;
-        continue;
-    }
-    if(a){ 
-        if(temp == ' '){
-            if (j == 0 && k==0)
-                continue;
-            if (j == 9){
-                if(k == 9){
-                    k = 0;
-                    j = 0;
-                }else{
-                    j = 0;
-                    k = k + 1;
-                }
-            }else{
-               j = j + 1; 
-            }
-        }else{
-            mat[i][j] = mat[i][j] * 10 + (temp - '0');
-    }
-    if(b){
-        if(temp == ' ')
-            continue;
-        if(source == -1){
-            source = (temp - '0');
-        }else{
-            ifRoute(source, (temp - '0'), mat);
-            source = -1;
+    char temp;
+    while(temp != 'D'){
+        scanf("%c", temp);
+        if(temp == 'A'){
+            fill_mat(mat);
         }
+        if(temp == 'B'){
+            route(0, mat);
+        }
+        if(temp == 'C'){
+            route(1, mat);
         }
     }
-    if(c){
-        if(temp == ' ')
-            continue;
-        if(source == -1){
-            source = (temp - '0');
-        }else{
-            printRoute(source, (temp - '0'), mat);
-            source = -1;
-        }
-    }
-}
 }
