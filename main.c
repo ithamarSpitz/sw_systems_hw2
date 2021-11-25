52899fa4a7458a7657fb49c10e33144cdc6b49e4
ithamarSpitz <itas12@gmail.com>
2021-11-25 16:53:53 +0200

    main

---
+++ main.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+void get_mat(mat[10][10]){
+
+}
+void replace2zeros(int mat[10][10]){
+  for (int i = 0; i < 10; i++){
+    for(int j = 0; j < 10 ; j++){
+        mat[i][j] = 0;
+    }
+  }
+}
+
+void main{
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    int mat[10][10];
+    replace2zeros(*mat)
+    int k = 0;
+    int j = 0;
+    char str[10000];
+    scanf(%s, str);
+    int index = 0;
+for (size_t i = 0; i < count; i++)
+{
+    char temp = str[i]
+    if(temp == 'A'){
+        a = 1;
+        b = 0;
+        c = 0;
+        continue;
+    }
+    if(temp == 'B'){
+        a = 1;
+        b = 0;
+        c = 0;
+        continue;
+    }
+    if(temp == 'C'){
+        a = 1;
+        b = 0;
+        c = 0;
+        continue;
+    }
+    if(a){ 
+        if(temp == ' '){
+            if (j == 9){
+                if(k == 9){
+                    k = 0;
+                    j = 0;
+                }else{
+                    j = 0;
+                    k = k + 1;
+                }
+            }
+            
+        }
+        mat[i][j] = 
+
+    }
+    if(b)
+    if(c)
+}
+
+    
+}
